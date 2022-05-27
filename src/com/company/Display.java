84a9d787eade1c283db90685edd6da93b8f43da4
Myrzabek Alizhanov <mlgexgood@gmail.com>
2022-05-27 10:25:42 +0600

    Game Loop

--- src/com/company/Display.java
+++ src/com/company/Display.java
@@ -3,9 +3,42 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 
-public class Display extends Canvas {
+public class Display extends Canvas implements Runnable {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
+
+    private Thread thread;
+    private boolean isRunning;
+
+    //todo: make in other class
+    private void start() {
+        if (isRunning)
+            return;
+
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
+    private void stop() {
+        if (!isRunning)
+            return;
+        isRunning = false;
+
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+        }
+    }
 
     public static void main(String[] args) {
         Display game = new Display();
@@ -18,5 +51,8 @@ public class Display extends Canvas {
         frame.setVisible(true);
 
         System.out.println("Running...");
+        game.start();
     }
+
+
 }
